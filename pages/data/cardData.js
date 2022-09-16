6f5cc77d3176e4c10e0dff68ce27d9f0b52ad2b2
Amit Bar-Gil <amit217@yandex.com>
2022-09-16 15:05:14 +0300

    Test deploy

--- pages/data/cardData.js
+++ pages/data/cardData.js
@@ -7,11 +7,11 @@ export const cards = [
         projectLink: "https://github.com/AmitGit217/react-around-auth",
     },
 
-    {
-        frontTitle: "Around the U.S",
-        frontImage: "/../public/assets/vector.png",
-        backDescription: `A full stack web application for uploading, updating, and liking images. 
-            also for editing the main profile data and image. This app was developed using: React, Node + Express and MongoDB `,
-        projectLink: "https://github.com/AmitGit217/react-around-auth",
-    },
+    // {
+    //     frontTitle: "Around the U.S",
+    //     frontImage: "/../public/assets/vector.png",
+    //     backDescription: `A full stack web application for uploading, updating, and liking images.
+    //         also for editing the main profile data and image. This app was developed using: React, Node + Express and MongoDB `,
+    //     projectLink: "https://github.com/AmitGit217/react-around-auth",
+    // },
 ];
